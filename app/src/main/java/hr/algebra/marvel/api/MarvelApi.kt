893a2e3f530d44ef56faa1a6f6aa8fe13a7c5df5
Mancek <52491015+Mancek@users.noreply.github.com>
2022-02-09 17:19:08 +0100

    Update MarvelApi.kt

--- app/src/main/java/hr/algebra/marvel/api/MarvelApi.kt
+++ app/src/main/java/hr/algebra/marvel/api/MarvelApi.kt
@@ -7,8 +7,8 @@ import retrofit2.http.Query
 import java.time.Instant
 
 const val API_URL = "https://gateway.marvel.com:443/v1/public/"
-const val PRIVATE_API_KEY = "fd03cf1cd381064e3b2e98056dabeb29d867e994"
-const val PUBLIC_API_KEY = "d43e2bafa38eb515ae349200b40f5c25"
+const val PRIVATE_API_KEY = "key"
+const val PUBLIC_API_KEY = "key"
 
 interface MarvelApi {
     @GET("characters")
